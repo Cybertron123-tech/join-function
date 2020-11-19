407c2f8031c98cca33db309fa1be42564677bebc
Cybertron123-tech <71644024+Cybertron123-tech@users.noreply.github.com>
2020-11-19 17:14:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function getParagraph1() {
+    var inputs = []
+    for(var i = 1 ; i <=6 ; i++);
+    inputs.join(". ");
+    document.getElementById("showParagraph").innerHTML = inputs.join(". ");
+}
